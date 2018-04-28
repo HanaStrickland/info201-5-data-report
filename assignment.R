d6ca5023e891fc707c84846183097a518ea2bc65
hanastrickland <strickh@uw.edu>
2018-04-28 12:08:31 -0700

    complete part 1

--- assignment.R
+++ assignment.R
@@ -1,16 +1,45 @@
-source(" /api_key.R")
+library(dplyr)
+library(httr)
+library(jsonlite)
+#install.packages(knitr)
+#library(knitr)
+
+
+# Get Data
+source("api_key.R")
+
+subject <- "taxes"
 
 base_uri <- "https://api.propublica.org/congress/v1/"
 
-# Get Data
-response <- GET(paste0(base_uri, "bills/subjects/climate.json"), 
+resource <- "bills/search.json"
+
+query_params <- list(query = subject)
+
+response <- GET(paste0(base_uri, resource), query = query_params,
                 add_headers("X-API-Key" = propublica_key))
 
-climate_body <- content(response, "text")
+subject_body <- content(response, "text")
 
-parsed_climate_body <- fromJSON(climate_body) # turn climate_body string into a list
 
-recent_climate_bills <- data.frame(parsed_climate_body$results, stringsAsFactors = FALSE) # Turn list into data frame
+parsed_subject_body <- fromJSON(subject_body)
 
-recent_climate_bills <- flatten(recent_climate_bills)
-recent_climate_bills
+
+recent_subject_bills <- data.frame(parsed_subject_body$results, stringsAsFactors = FALSE) # Turn list into data frame
+
+bills <- recent_subject_bills$bills
+
+bills <- as.data.frame((bills))
+
+bills <- flatten(bills)
+
+
+# Filter Data
+subject_df <- bills %>% 
+  select(bill_id, short_title, sponsor_name, sponsor_state, sponsor_party, latest_major_action, latest_major_action_date, congressdotgov_url) %>% 
+  mutate(sponsor = paste(sponsor_name, sponsor_state, sponsor_party, sep = ", ")) %>% 
+  select(bill_id, short_title, sponsor, latest_major_action, latest_major_action_date, congressdotgov_url) 
+
+subject_df <- subject_df[1:10, ] # Filters for 10 most recent bills
+
+# Time: 2:40 
