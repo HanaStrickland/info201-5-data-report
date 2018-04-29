948dabc0849a2a85598a2c03b3a2385e666a74ba
hanastrickland <strickh@uw.edu>
2018-04-29 16:35:01 -0700

    Complete downloads for part 1,2,3

--- assignment.R
+++ assignment.R
@@ -1,45 +1,105 @@
-library(dplyr)
-library(httr)
-library(jsonlite)
-#install.packages(knitr)
-#library(knitr)
 
-
-# Get Data
+# Get Bills on Topic
 source("api_key.R")
 
-subject <- "taxes"
+topic <- "taxation"
 
 base_uri <- "https://api.propublica.org/congress/v1/"
 
-resource <- "bills/search.json"
+resource_topic <- "bills/search.json"
 
-query_params <- list(query = subject)
+query_params_topic <- list(query = topic)
 
-response <- GET(paste0(base_uri, resource), query = query_params,
+response_topic <- GET(paste0(base_uri, resource_topic), query = query_params_topic,
                 add_headers("X-API-Key" = propublica_key))
 
-subject_body <- content(response, "text")
+topic_body <- content(response_topic, "text")
 
 
-parsed_subject_body <- fromJSON(subject_body)
+parsed_topic_body <- fromJSON(topic_body)
 
 
-recent_subject_bills <- data.frame(parsed_subject_body$results, stringsAsFactors = FALSE) # Turn list into data frame
+recent_topic_bills <- data.frame(parsed_topic_body$results, stringsAsFactors = FALSE) # Turn list into data frame
 
-bills <- recent_subject_bills$bills
+bills <- recent_topic_bills$bills
 
 bills <- as.data.frame((bills))
 
 bills <- flatten(bills)
 
+## Filter Data
+topic_df <- bills %>% 
+  select(bill_id, short_title, sponsor_name, sponsor_state, sponsor_party, cosponsors, latest_major_action, congressdotgov_url) %>% 
+  mutate(sponsor = paste(sponsor_name, sponsor_state, sponsor_party, cosponsors , sep = ", ")) %>%
+  select(bill_id, short_title, sponsor, latest_major_action, congressdotgov_url) 
 
-# Filter Data
-subject_df <- bills %>% 
-  select(bill_id, short_title, sponsor_name, sponsor_state, sponsor_party, latest_major_action, latest_major_action_date, congressdotgov_url) %>% 
-  mutate(sponsor = paste(sponsor_name, sponsor_state, sponsor_party, sep = ", ")) %>% 
-  select(bill_id, short_title, sponsor, latest_major_action, latest_major_action_date, congressdotgov_url) 
+topic_df <- topic_df[1:10, ] # Filters for 10 most recent bills, Probublica default is chronological in decending order
 
-subject_df <- subject_df[1:10, ] # Filters for 10 most recent bills
+# Get Representative Contact Info
+representative_id <- "S000510"
 
-# Time: 2:40 
+resource_rep <- paste0("members/", representative_id, ".json")
+
+response_rep <- GET(paste0(base_uri, resource_rep), 
+                add_headers('X-API-Key' = propublica_key)) 
+
+member_body <- content(response_rep, "text")
+
+parsed_member_body <- fromJSON(member_body)
+
+member <- as.data.frame(parsed_member_body$results)
+
+member <- flatten(member)
+
+roles_df <- as.data.frame(member$roles)
+
+# Get Bills sponsored by rep
+
+resource_sponsored <- paste0("members/", representative_id , "/bills/introduced.json")
+
+response_sponsored <- GET(paste0(base_uri, resource_sponsored), 
+                          add_headers('X-API-Key' = propublica_key)) 
+
+sponsored_body <- content(response_sponsored, "text")
+
+parsed_sponsored_body <- fromJSON(sponsored_body)
+
+sponsored_bills <- as.data.frame(parsed_sponsored_body)
+
+sponsored_bills <- flatten(sponsored_bills)
+
+sponsored_bills <- as.data.frame(sponsored_bills$results.bills)
+
+recent_sponsored_bills <- sponsored_bills[1:10, ]
+
+recent_sponsored_bills <- 
+  recent_sponsored_bills %>% 
+  select(bill_id, short_title)
+
+# Get bills cosponsored by rep
+source("api_key.R")
+
+cosponsored_resource <- paste0("members/",representative_id , "/bills/cosponsored.json")
+
+response_cosponsored <- GET(paste0(base_uri, cosponsored_resource), 
+                          add_headers('X-API-Key' = propublica_key)) 
+
+cosponsored_body <- content(response_cosponsored, "text")
+
+parsed_cosponsored_body <- fromJSON(cosponsored_body)
+
+cosponsored_bills <- as.data.frame(parsed_cosponsored_body)
+
+cosponsored_bills <- flatten(cosponsored_bills)
+
+cosponsored_bills <- as.data.frame((cosponsored_bills$results.bills))
+
+recent_cosponsored_bills <-  cosponsored_bills[1:10, ]
+
+recent_cosponsored_bills <- 
+  recent_cosponsored_bills %>% 
+  select(bill_id, short_title)
+
+# merge recent sponsored and cosponsored bills
+sponsored_and_cosponsored <- full_join(recent_sponsored_bills, recent_cosponsored_bills, by = c("bill_id" = "bill_id", "short_title" = "short_title"))
+
