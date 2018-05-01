6261ecbd5c96e067f09a427b28e9f4a11c65da65
hanastrickland <strickh@uw.edu>
2018-04-30 21:56:26 -0700

    add faa vote; put inline tags in .R file

--- assignment.R
+++ assignment.R
@@ -1,19 +1,29 @@
-### Get Bills ###
-
 source("api_key.R") # source api_key
 
+### Packaged Functions ###
 # package base uri into a a function for use multiple times
 base_uri <- function() {
   get_base_uri <- "https://api.propublica.org/congress/v1/"
-  get_base_uri
 }
 
-# Function gets data frame of all recent bills for chosen topic
-bills_on_topic <- function() { 
+# Function api response, body, and parsed_body into a function for multiple use
+get_parsed_body <- function(resource) {
+  response <- GET(
+    paste0(base_uri(), resource),
+    add_headers("X-API-Key" = propublica_key)
+  )
 
+  body <- content(response, "text")
+
+  parsed_body <- fromJSON(body)
+}
+
+### Get All Recent Bills ###
+# Function gets data frame of all recent bills for chosen topic
+bills_on_topic <- function() {
   resource_topic <- "bills/search.json"
 
-  query_params_topic <- list(query = topic) 
+  query_params_topic <- list(query = topic)
 
   response_topic <- GET(paste0(base_uri(), resource_topic),
     query = query_params_topic,
@@ -24,22 +34,20 @@ bills_on_topic <- function() {
 
   parsed_topic_body <- fromJSON(topic_body)
 
-  recent_topic_bills <- data.frame(parsed_topic_body$results, stringsAsFactors = FALSE) 
+  recent_topic_bills <- data.frame(parsed_topic_body$results, stringsAsFactors = FALSE)
 
   bills <- recent_topic_bills$bills
 
   bills <- as.data.frame((bills))
 
   bills <- flatten(bills)
-
-  bills
 }
 
-df_bills_on_topic <- bills_on_topic() 
+df_bills_on_topic <- bills_on_topic()
 
+### Get Select Recent Bills for Report ###
 # Function gets select columns for the ten most recent bills on chosen topic from above
 bills_select_col <- function() {
-
   topic_df <- df_bills_on_topic %>%
     select(bill_id, short_title, sponsor_name, sponsor_state, sponsor_party, cosponsors, latest_major_action, congressdotgov_url) %>%
     mutate(sponsor = paste(sponsor_name, sponsor_state, sponsor_party, cosponsors, sep = ", ")) %>%
@@ -50,21 +58,24 @@ bills_select_col <- function() {
 
 bills_select_df <- bills_select_col()
 
+### in-line names for .Rmd file (so the in-line code remains short and readable) ###
+
+faa_bill <- df_bills_on_topic %>% 
+  filter(short_title == "FAA Reauthorization Act of 2018") 
+
+bill_name <- faa_bill$short_title
+bill_long_title <- faa_bill$title
+bill_sponsor <- faa_bill$sponsor_name
+sponsor_party <- faa_bill$sponsor_party
+sponsor_state <- faa_bill$sponsor_state
+cosponsors <- faa_bill$cosponsors
+latest_major_action <- faa_bill$latest_major_action
+link_to_text <- faa_bill$congressdotgov_url
+passed_house <- faa_bill$house_passage
+
+
+
 ### Get Representative Contact Info ###
-
-#Function api response, body, and parsed_body into a function for multiple use
-get_parsed_body <- function(resource) {
-
-  response <- GET(
-    paste0(base_uri(), resource),
-    add_headers("X-API-Key" = propublica_key))
-
-  body <- content(response, "text")
-
-  parsed_body <- fromJSON(body)
-  parsed_body
-
-}
 
 get_rep_info <- function() {
   resource_rep <- paste0("members/", representative_id, ".json")
@@ -74,26 +85,19 @@ get_rep_info <- function() {
   member <- as.data.frame(parsed_member_body$results)
 
   member <- flatten(member)
-  member
-
 }
 
 member <- get_rep_info()
 
-
-
-
-# Function gets more contact info nested in roles column 
-get_rep_roles <- function(){
-  
+# Function gets more contact info nested in roles column
+get_rep_roles <- function() {
   member
   roles_df <- as.data.frame(member$roles)
-  roles_df
 }
 
 rep_roles_df <- get_rep_roles()
 
-### Get Bills sponsored by rep ###
+### Get Bills Sponsored By Rep ###
 
 resource_sponsored <- paste0("members/", representative_id, "/bills/introduced.json")
 
@@ -105,13 +109,13 @@ sponsored_bills <- flatten(sponsored_bills)
 
 sponsored_bills <- as.data.frame(sponsored_bills$results.bills)
 
-recent_sponsored_bills <- sponsored_bills[1:10, ]
+recent_sponsored_bills <- sponsored_bills[1:5, ]
 
 recent_sponsored_bills <-
   recent_sponsored_bills %>%
   select(bill_id, short_title)
 
-### Get bills cosponsored by rep ###
+### Get Bills Cosponsored By Rep ###
 
 cosponsored_resource <- paste0("members/", representative_id, "/bills/cosponsored.json")
 
@@ -123,7 +127,7 @@ cosponsored_bills <- flatten(cosponsored_bills)
 
 cosponsored_bills <- as.data.frame(cosponsored_bills$results.bills)
 
-recent_cosponsored_bills <- cosponsored_bills[1:10, ]
+recent_cosponsored_bills <- cosponsored_bills[1:5, ]
 
 recent_cosponsored_bills <-
   recent_cosponsored_bills %>%
@@ -134,9 +138,8 @@ sponsored_and_cosponsored <- full_join(recent_sponsored_bills, recent_cosponsore
 
 # Voted with majority of other party
 
-### Get all recent votes ###
-
-
+### Get All Recent Votes ###
+# get roll call votes that are majority republican position
 resource_recent_votes <- "house/votes/recent.json"
 
 parsed_recent_votes_body <- get_parsed_body(resource_recent_votes)
@@ -164,21 +167,20 @@ rep_votes_df <- flatten(rep_votes_df)
 
 rep_votes_df$roll_call <- as.integer(rep_votes_df$roll_call) # Need to convert into integer to match recent_votes
 
-
 select_rep_votes <-
   rep_votes_df %>%
   select(roll_call, bill.title, position)
 
-## Merge all votes and rep votes
-
+# Merge all votes and rep votes
 compare_voting_record <- left_join(select_rep_votes, select_bills_majority_republican, by = c("roll_call" = "votes.roll_call"))
 
+# Get the number roll call votes with a republican majority position
 filter_yes_republican_majority_position <-
   compare_voting_record %>%
   filter(votes.republican.majority_position == "Yes")
 
 num_yes_republican_majority_position <- nrow(filter_yes_republican_majority_position)
-
+# get the number of representative's yes votes out of roll call votes with republican majority position
 filter_yes_rep_vote <-
   filter_yes_republican_majority_position %>%
   filter(position == "Yes")
@@ -187,5 +189,20 @@ num_yes_rep_vote <- nrow(filter_yes_rep_vote)
 
 pct_rep_vote_with_other_party <- num_yes_rep_vote / num_yes_republican_majority_position
 
-pct_rep_vote_with_other_party <- round(100 * pct_rep_vote_with_other_party) # in percentage
+pct_rep_vote_with_other_party <- round(100 * pct_rep_vote_with_other_party) # format in percentage
 
+
+# Get FAA Reauthorization Act House Vote 
+filter_faa_vote <- recent_votes %>% 
+  filter(votes.roll_call == "165") %>% 
+  select(votes.question, votes.bill.bill_id, votes.total.yes, votes.total.no)
+
+yes_faa <- filter_faa_vote$votes.total.yes
+no_faa <- filter_faa_vote$votes.total.no
+
+filter_faa_roll_call <- recent_votes %>% 
+  filter(votes.bill.bill_id == "hr4-115") %>% 
+  select(votes.question, votes.bill.bill_id, votes.total.yes, votes.total.no)
+
+num_times_faa_voted_on <- nrow(filter_faa_roll_call)
+  
